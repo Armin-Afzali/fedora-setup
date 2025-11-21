19b69b70621e84f5a18605e1ecf0e4f0398f7931
ARMIN11DSE@gmail.com <armin11dse@gmail.com>
2025-11-21 09:24:44 +0330

    command fix, removed "$" from beginning.

--- throne-installer.sh
+++ throne-installer.sh
@@ -1,2 +1,2 @@
-$ sudo curl -o /etc/yum.repos.d/throne.repo https://parhelia512.github.io/throne.repo
-$ sudo dnf install -y throne --refresh
+sudo curl -o /etc/yum.repos.d/throne.repo https://parhelia512.github.io/throne.repo
+sudo dnf install -y throne --refresh
